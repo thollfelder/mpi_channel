3bf3e6fcd5878e01e566099644445240531d33d1
Toni Hollfelder <toli.h@gmx.de>
2021-06-15 17:40:46 +0200

    updated

--- test_job_slurm.sh
+++ test_job_slurm.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-#SBATCH -J job_001
-#SBATCH -o job.%j.out 
-#SBATCH -N 1 
-#SBATCH --ntasks-per-node 8 
-#SBATCH -t 01:30:00 
-#SBATCH --nodelist node20
-#SBATCH --exclusive
+#SBATCH --job-name job_001                              # Specifies name for job allocation; default is script name
+# --output job.%j.out 
+#SBATCH --nodes 1                                       # Required number of nodes
+#SBATCH --ntasks-per-node 8                             # Maximum processes per node
+#SBATCH --time 00:05:00                                 # Sets a time limit
+#SBATCH --nodelist node20                               # Requests a specific list of hosts
+#SBATCH --exclusive                                     # Job allocation does not share node with others
+#SBATCH --mail-type=END,FAIL                            # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=toni.hollfelder@uni-bayreuth.de     # Where to send mail
 
 echo "SLURM_CLUSTER_NAME:$SLURM_CLUSTER_NAME"
 echo "SLURM_CPU_PER_TASK:$SLURM_CPU_PER_TASK"
