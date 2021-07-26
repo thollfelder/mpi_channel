7c7578108b23e96e7a9177a1a4bcf4ea56060c1d
thollfelder <toli.h@gmx.de>
2021-07-26 22:00:31 +0200

    Delete thrash directory

--- thrash/MPI_Channel_Basic.c
+++
@@ -1,7 +0,0 @@
-#include "MPI_Channel_Basic.h"
-
-static int tag_counter = 0;
-
-int get_tag_from_counter() {
-    return tag_counter++;
-}
--- thrash/TestStructure/Test.c
+++
@@ -1,6 +0,0 @@
-#include "Chan.h"
-
-void main() {
-    Channel *chan = create_something(-1);
-
-}
